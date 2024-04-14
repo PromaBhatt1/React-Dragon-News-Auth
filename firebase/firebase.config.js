f92b8fa074ae2da222432a200de089ca4b240844
promabhatt1 <promabhattacharjee1proff@gmail.com>
2024-04-14 09:39:18 +0600

    added firebase env file

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDjjKIZTzlpJ3GvLazd36g89c_8dzlGAJ8",
-  authDomain: "react-dragon-news-auth-ee75d.firebaseapp.com",
-  projectId: "react-dragon-news-auth-ee75d",
-  storageBucket: "react-dragon-news-auth-ee75d.appspot.com",
-  messagingSenderId: "388051064589",
-  appId: "1:388051064589:web:b5e4afe080051378090244"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
